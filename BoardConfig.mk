79e7afaab7d9cde4eebdfcf62d33a383dc207126
kallt kaffe <kallt_kaffe@apedroid.com>
2012-10-20 21:44:31 +0200

    Catch up with pershoot

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,6 +22,9 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Use the non-open-source parts, if they're present
 -include vendor/samsung/p5wifi/BoardConfigVendor.mk
 
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 699744256
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 14080704512
+
 TARGET_OTA_ASSERT_DEVICE := p5wifi,p5,GT-P7300,GT-P7310
 
 TARGET_PREBUILT_KERNEL := device/samsung/p5wifi/kernel
--- p5wifi.mk
+++ p5wifi.mk
@@ -21,6 +21,9 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/modules/dhd.ko:system/lib/modules/dhd.ko
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # The gps config appropriate for this device
@@ -45,7 +48,16 @@ PRODUCT_COPY_FILES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/gps.xml:system/etc/gps.xml
+
+# GPS config
+PRODUCT_PROPERTY_OVERRIDES += \
+  my.gps=novzw
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := p5wifi
 PRODUCT_DEVICE := p5wifi
 PRODUCT_MODEL := p5wifi
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
